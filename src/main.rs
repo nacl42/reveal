b6955859b7a5df2691cb274ee2de430895880066
sreblov <code@sreblov.de>
2021-05-14 23:02:20 +0200

    added simple map drawing via tileset

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use macroquad::{input, window};
 mod effect;
 use effect::{TextEffect, ScaleText};
 
+mod tileset;
+use tileset::{Tileset, Pattern};
 
 fn window_conf() -> Conf {
     Conf {
@@ -27,8 +29,17 @@ async fn main() {
         ..Default::default()
     };
 
+    // sample text effect (proof of concept)
     let mut effects: Vec<Box<dyn TextEffect>> = vec!();
-    
+
+    // pattern and tileset
+    let (width, height) = (32.0, 32.0);
+    let pattern = Pattern::Matrix {
+        width, height,
+        rows: 2, columns: 2
+    };
+    let tileset = Tileset::new("assets/tileset32.png", &pattern).await.unwrap();
+
     // main loop
     let mut last_update = get_time();
     const DELTA: f64 = 0.05;
@@ -67,8 +78,43 @@ async fn main() {
 
         // redraw
         clear_background(WHITE);
-        draw_text_ex("Reveal - Mystic Land of Magic and Adventure", x, y, params);
+
+        // draw map
+        let (base_x, base_y) = (50.0, 50.0);
+        let (sep_x, sep_y) = (1.0, 1.0);
+        let (tiles_x, tiles_y) = (16, 12);
+        let mut py = base_y;
         
+        for y in 0..tiles_y {
+            let mut px = base_x;
+            for x in 0..tiles_x {
+                px += width + sep_x;
+                if let Some(index) = Some(&0) {
+                    match tileset.sources.get(*index) {
+                        Some(&source) => {
+                            let mut params = DrawTextureParams {
+                                dest_size: Some(Vec2::new(width, height)),
+                                source: Some(source),
+                                ..Default::default()
+                            };
+
+                            draw_texture_ex(
+                                tileset.texture, px, py, WHITE,
+                                params
+                            )
+                        },
+                        _ => {}
+                    }
+                };
+            }
+            py += height + sep_y;
+        }
+
+        // draw text
+        draw_text_ex(
+            "Reveal - Mystic Land of Magic and Adventure", x, y, params
+        );
+
         next_frame().await
     }
 }
---
+++ src/tileset.rs
@@ -0,0 +1,60 @@
+use macroquad::prelude::*;
+
+/// A Pattern defines how the source images are taken from a Tileset.
+/// The Pattern::Matrix defines a rectangular matrix of source rects
+/// width a given `width` and `height` for each rect.
+pub enum Pattern {
+    Matrix { rows: u16, columns: u16, width: f32, height: f32 }
+}
+
+impl Pattern {
+    pub fn width(&self) -> f32 {
+        match self {
+            Pattern::Matrix { width, .. } => width.clone()
+        }
+    }
+
+    pub fn height(&self) -> f32 {
+        match self {
+            Pattern::Matrix { height, .. } => height.clone()
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Tileset {
+    pub texture: Texture2D,
+    pub sources: Vec<Rect>
+}
+
+
+impl Tileset {
+    pub async fn new(filename: &'_ str, pattern: &Pattern)
+                     -> Result<Tileset, ()>
+    {
+        let texture: Texture2D = load_texture(filename).await.unwrap();
+
+        // set sources according to tileset Pattern
+        let mut sources = Vec::<Rect>::new();
+        match pattern {
+            &Pattern::Matrix { rows, columns, width, height } => {
+                for x in 0..columns {
+                    for y in 0..rows {
+                        sources.push(Rect::new(
+                            x as f32 * width, y as f32 * height,
+                            width, height
+                        ));
+                    }
+                }
+            }
+        }
+
+        Ok(
+            Tileset {
+                texture,
+                sources
+            }
+        )
+    }
+}
