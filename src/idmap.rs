1e8b137061ae6f94dab0eeac761326087d3ac92d
sreblov <code@sreblov.de>
2021-05-25 21:26:39 +0200

    improved IdMap to make it usable as replacement for HashMap<Id<T>, T>

--- src/idmap.rs
+++ src/idmap.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::collections::hash_map::{Iter, IterMut, Values, ValuesMut};
+
 use crate::id::Id;
 
 #[derive(Debug)]
@@ -8,6 +10,7 @@ pub struct IdMap<T> {
 
 impl<T> IdMap<T> {
 
+    /// Set up a new, empty IdMap.
     #[allow(dead_code)]
     pub fn new() -> Self {
         Self {
@@ -15,16 +18,69 @@ impl<T> IdMap<T> {
         }
     }
 
+    /// Insert the given value of type T into the IdMap.
+    /// Returns the newly created key which is of type Id<T>.
     #[allow(dead_code)]
     pub fn add(&mut self, value: T) -> Id<T> {
         let key = Id::new();
         self.map.insert(key.clone(), value);
         key
     }
+
+    /// Get a reference to the value with the given id.
+    pub fn get(&self, id: &Id<T>) -> Option<&T> {
+        self.map.get(id)
+    }
+
+    /// Get a mutable reference to the value with the given id.
+    pub fn get_mut(&mut self, id: &Id<T>) -> Option<&mut T> {
+        self.map.get_mut(id)
+    }
+
+    /// Return an iterator over all (id, value) pairs.
+    pub fn iter(&self) -> Iter<'_, Id<T>, T> {
+        self.map.iter()
+    }
+
+    /// Return a mutable iterator over all (id, value) pairs
+    pub fn iter_mut(&mut self) -> IterMut<'_, Id<T>, T> {
+        self.map.iter_mut()
+    }
+
+    /// Return an iterator over all values.
+    pub fn values(&self) -> Values<'_, Id<T>, T> {
+        self.map.values()
+    }
+
+    /// Return a mutable iterator over all values.
+    pub fn values_mut(&mut self) -> ValuesMut<'_, Id<T>, T> {
+        self.map.values_mut()
+    }
+}
+
+
+impl <'a, T> IntoIterator for &'a IdMap<T> {
+    type Item = (&'a Id<T>, &'a T);
+    type IntoIter = Iter<'a, Id<T>, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.map.iter()
+    }
+}
+
+
+impl <'a, T> IntoIterator for &'a mut IdMap<T> {
+    type Item = (&'a Id<T>, &'a mut T);
+    type IntoIter = IterMut<'a, Id<T>, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.map.iter_mut()
+    }
 }
 
 
 
+// TODO: write tests for IdMap
 #[cfg(tests)]
 mod tests {
     #[test]
